9f0309d438d17d10d077fd9e309936e61d40effb
Tomohiro Kaneda <tomokane@dream.email.ne.jp>
2024-07-22 14:06:34 +0900

    202407221406

--- pythondocs/japanholiday/JapanHoliday.py
+++ pythondocs/japanholiday/JapanHoliday.py
@@ -1,18 +1,37 @@
-from datetime import date
+import datetime
 
-class Holiday(object):
-
-    __slots__ = ['date', 'year', 'month', 'day_of_month', 'holiday_name']
+class JapanHoliday(object):
 
     def __init__(self, year: int, month: int, day_of_month: int, holiday_name: str):
-        self.date = date(year, month, day_of_month)
-        self.year = year
-        self.month = month
-        self.day_of_month = day_of_month
-        self.holiday_name = holiday_name
+        self.__date = datetime.date(year, month, day_of_month)
+        self.__year = year
+        self.__month = month
+        self.__day_of_month = day_of_month
+        self.__holiday_name = holiday_name
+
+    @property
+    def date(self) -> datetime.date:
+        return self.__date
+
+    @property
+    def year(self) -> int:
+        return self.__year
+
+    @property
+    def month(self) -> int:
+        return self.__month
+
+    @property
+    def day_of_month(self) -> int:
+        return self.__day_of_month
+
+    @property
+    def holiday_name(self) -> str:
+        return self.__holiday_name
+
 
     def __str__(self):
-        return f'{self.date} {self.holiday_name}'
+        return f'{self.__date} {self.__holiday_name}'
 
 
 
--- pythondocs/main.py
+++ pythondocs/main.py
@@ -1,36 +1,35 @@
 import csv
 
-from japanholiday.Holiday import Holiday
+from japanholiday.JapanHoliday import JapanHoliday
 
-holiday_years = {}
+holidays = {}
 
 with open('syukujitsu.csv', newline='', encoding='sjis') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    holidays = {}
-    for row in spamreader:
+    csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in csv_reader:
         if row[0][0].isnumeric():
             date_source = row[0].split('/')
             year = int(date_source[0])
             month = int(date_source[1])
             day_of_month = int(date_source[2])
             holiday_name = row[1]
-            hol = Holiday(year=year, month=month, day_of_month=day_of_month, holiday_name=holiday_name)
-            if not holiday_years.get(year):
-                holiday_years[year] = []
-            holiday_years[year].append(hol)
+            hol = JapanHoliday(year=year, month=month, day_of_month=day_of_month, holiday_name=holiday_name)
+            if not holidays.get(year):
+                holidays[year] = []
+            holidays[year].append(hol)
 
 def main():
-    holidays = get_holidays(2024)
-    for h in holidays:
-        print(h)
+    _holidays = get_holidays(2024)
+    for h in _holidays:
+        print(f'year: {h.year}')
 
 
-def get_holidays(year: int, month: int = None) -> list[Holiday]:
+def get_holidays(year: int, month: int = None) -> list[JapanHoliday]:
     if not month:
-        return holiday_years.get(year)
+        return holidays.get(year)
     else:
         holiday_months = []
-        all_the_year = holiday_years.get(year)
+        all_the_year = holidays.get(year)
         if all_the_year:
             for holiday in all_the_year:
                 if holiday.month == month:
@@ -38,8 +37,8 @@ def get_holidays(year: int, month: int = None) -> list[Holiday]:
         return holiday_months
 
 
-def get_holiday(year: int, month: int, date: int) -> Holiday | None:
-    all_the_year = holiday_years.get(year)
+def get_holiday(year: int, month: int, date: int) -> JapanHoliday | None:
+    all_the_year = holidays.get(year)
     if all_the_year:
         for holiday in all_the_year:
             if holiday.month == month:
